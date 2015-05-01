faa7609c7aad5c775197cefca99c20b79ce85670
Dale Ragan <dale@ragan.io>
2015-05-01 13:48:17 -0400

    Updating intelliweather.js to 0.1.2-beta release.

--- javascripts/jquery.intelliweather-0.1.2-beta.js
+++ javascripts/jquery.intelliweather-0.1.2-beta.js
@@ -1,5 +1,5 @@
 /*!
- * IntelliWeather.js 0.1.1-beta
+ * IntelliWeather.js 0.1.2-beta
  * http://www.intelliweather.com
  * Copyright 2014 IntelliWeather, Inc.
  */
@@ -100,6 +100,17 @@
                 paddingLeft: "50px",
                 paddingRight: "50px"
             },
+            iwMinus: {
+                cursor: "pointer",
+                padding: "8px"
+            },
+            iwSpeedIndicator: {
+                padding: "5px"
+            },
+            iwPlus: {
+                cursor: "pointer",
+                padding: "8px"
+            },
             overlay: {
                 position: "fixed",
                 zIndex: "500",
@@ -146,6 +157,9 @@
             previousControl: '<i class="iw-prev fa fa-backward fa-2x"></i>',
             pausePlayControl: '<i class="iw-pauseplay fa fa-pause fa-2x"></i>',
             nextControl: '<i class="iw-next fa fa-forward fa-2x"></i>',
+            minusControl: '<i class="iw-minus fa fa-minus"></i>',
+            speedIndicator: '<i class="fa fa-circle-o"></i>',
+            plusControl: '<i class="iw-plus fa fa-plus"></i>',
             overlay: '<div id="overlay"></div>',
             modalHeader: '<div class="header"></div>',
             modalClose: '<i class="fa fa-close fa-2"></i>',
@@ -259,28 +273,72 @@
             });
         }
         _.extend(Carousel.prototype, {
+            _setupSpeedControl: function setupSpeedControl() {
+                var $speedControl = $('<div id="speedControl"></div>');
+                var pauseTime = this.settings.pauseTime;
+                var percentage = pauseTime * .2;
+                var pauseTimes = [];
+                pauseTimes[0] = pauseTime + percentage * 2;
+                pauseTimes[1] = pauseTime + percentage;
+                pauseTimes[2] = pauseTime;
+                pauseTimes[3] = pauseTime - percentage;
+                pauseTimes[4] = pauseTime - percentage * 2;
+                this.currentPauseTimeIndex = 2;
+                this.$minusControl = $(html.minusControl).css(css.iwMinus).appendTo($speedControl);
+                for (var i = 0; i < pauseTimes.length; i++) {
+                    var $indicator = $(html.speedIndicator).attr("id", "ind-" + i).css(css.iwSpeedIndicator).appendTo($speedControl);
+                    if (pauseTime <= pauseTimes[i]) {
+                        $indicator.removeClass("fa-circle-o").addClass("fa-dot-circle-o");
+                    }
+                }
+                this.$plusControl = $(html.plusControl).css(css.iwPlus).appendTo($speedControl);
+                var that = this;
+                this.$minusControl.click(function() {
+                    if (that.currentPauseTimeIndex - 1 >= 0) {
+                        var selector = "#ind-" + that.currentPauseTimeIndex;
+                        $(selector).removeClass("fa-dot-circle-o").addClass("fa-circle-o");
+                        that.currentPauseTimeIndex = that.currentPauseTimeIndex - 1;
+                        that.settings.pauseTime = pauseTimes[that.currentPauseTimeIndex];
+                    }
+                });
+                this.$plusControl.click(function() {
+                    if (that.currentPauseTimeIndex + 1 < pauseTimes.length) {
+                        that.currentPauseTimeIndex = that.currentPauseTimeIndex + 1;
+                        var selector = "#ind-" + that.currentPauseTimeIndex;
+                        $(selector).removeClass("fa-circle-o").addClass("fa-dot-circle-o");
+                        that.settings.pauseTime = pauseTimes[that.currentPauseTimeIndex];
+                    }
+                });
+                return $speedControl;
+            },
+            _setupPlaybackControls: function setupPlaybackControls() {
+                var $playbackControls = $('<div id="playbackControls"></div>');
+                this.$previousControl = $(html.previousControl).css(css.iwPrevious).appendTo($playbackControls);
+                this.$pausePlayControl = $(html.pausePlayControl).css(css.iwPausePlay).appendTo($playbackControls);
+                this.$nextControl = $(html.nextControl).css(css.iwNext).appendTo($playbackControls);
+                var that = this;
+                this.$previousControl.click(function() {
+                    that.pause();
+                    that.prev();
+                });
+                this.$pausePlayControl.click(function() {
+                    if (that.state == "paused") {
+                        that.play();
+                    } else {
+                        that.pause();
+                    }
+                });
+                this.$nextControl.click(function() {
+                    that.pause();
+                    that.next();
+                });
+                return $playbackControls;
+            },
             _setupControls: function setupControls() {
                 if (this.slides.length > 1 && this.settings.controlArea && this.$container.find(this.settings.controlArea).length) {
                     this.$controls = $(html.controls).css(css.iwControls);
-                    this.$previousControl = $(html.previousControl).css(css.iwPrevious).appendTo(this.$controls);
-                    this.$pausePlayControl = $(html.pausePlayControl).css(css.iwPausePlay).appendTo(this.$controls);
-                    this.$nextControl = $(html.nextControl).css(css.iwNext).appendTo(this.$controls);
-                    var that = this;
-                    this.$previousControl.click(function() {
-                        that.pause();
-                        that.prev();
-                    });
-                    this.$pausePlayControl.click(function() {
-                        if (that.state == "paused") {
-                            that.play();
-                        } else {
-                            that.pause();
-                        }
-                    });
-                    this.$nextControl.click(function() {
-                        that.pause();
-                        that.next();
-                    });
+                    this._setupPlaybackControls().appendTo(this.$controls);
+                    this._setupSpeedControl().appendTo(this.$controls);
                     this.$container.find(this.settings.controlArea).append(this.$controls);
                 }
             },
